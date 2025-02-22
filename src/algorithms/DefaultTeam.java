1b2c1e509e01881f205a65966c6a834abff194fe
Aeon <kheireddine.ounnoughi@etu.sorbonne-universite.fr>
2025-02-22 21:42:56 +0100

    Budget Works and i get a 21 result

--- src/algorithms/DefaultTeam.java
+++ src/algorithms/DefaultTeam.java
@@ -83,87 +83,95 @@ public class DefaultTeam {
     int[][] paths = calculShortestPaths(points, edgeThreshold);
     int budget = 1664;
     
-    ArrayList<Point> opti_hitPoints = calc_OptiHitPoints(points,hitPoints,paths,budget);
-    
+    ArrayList<Point> hitPoints_copy = (ArrayList<Point>)hitPoints.clone();
+    ArrayList<infoPoint> infoPoints = scoring_algorithm(points,hitPoints,paths,100);
+    Collections.sort(infoPoints, (e1,e2) -> Integer.compare(e1.nb_closePoints, e2.nb_closePoints));
+    int distance = 10000;
 	Kruskal k = new Kruskal();
-	ArrayList<Edge> mstEdges = k.kruskal(opti_hitPoints);
-
+	ArrayList<Edge> mstEdges = new ArrayList<Edge>();
 	Tree2D steinerTree= k.edgesToTree(mstEdges, hitPoints.get(0));
 
 	Tree2D newSteinerTree = Steiner(paths, steinerTree, points);
+	
+	int index = 0;
+    while(distance > budget) {
+    	System.out.println("distance = " + distance);
+    	Point removePoint = infoPoints.get(index).root;
+    	hitPoints_copy.remove(removePoint);
+    	mstEdges = k.kruskal(hitPoints_copy);
+    	steinerTree= k.edgesToTree(mstEdges, hitPoints.get(0));
 
+    	newSteinerTree = Steiner(paths, steinerTree, points);
+    	distance = (int)distanceCalculator(newSteinerTree);
+    	index++;
+    }
+
+	System.out.println("distance c: " + (int)distanceCalculator(newSteinerTree));
 	return newSteinerTree;
   }
   
   
-  public ArrayList<Point> calc_OptiHitPoints(ArrayList<Point> points,ArrayList<Point> hitPoints, int[][] paths,int budget){
-	  	System.out.println("calc Opti hit points running");
-	    Point main_point = hitPoints.get(0);
-	    
-	    ArrayList<PointPath> hitPaths = new ArrayList<PointPath>();
-	    
-	    for(int i = 1 ; i < hitPoints.size(); i++) {
-	    	Point startPoint = main_point;
-	    	Point nextPoint = new Point(-1,-1); // point that cant exist
-	    	Point endPoint = hitPoints.get(i);
-	    	int end_point_Index = points.indexOf(hitPoints.get(i));
-	    	double distance = 0;
-	    	ArrayList<Point> path = new ArrayList<Point>();
-	    	
-	    	path.add(startPoint);
-	    	while(!nextPoint.equals(endPoint)) {
-	    		int nextPoint_Index = paths[points.indexOf(startPoint)][end_point_Index];
-	    		nextPoint = points.get(nextPoint_Index);
-	    		
-	    		path.add(nextPoint);
-	    		distance += startPoint.distance(nextPoint);
-	    		
-	    		startPoint = nextPoint;
-
-	    	}
-	    	hitPaths.add(new PointPath(hitPoints.get(i),path, distance));
-	    }
-	    
-	    ArrayList<Point> opti_hitPoints = Greedy(hitPaths,budget);
-	    System.out.println("calc Opti hit points finished");
-	    System.out.println("opti hitPoint size : " + opti_hitPoints.size());
-	    return opti_hitPoints;
+  public double distanceCalculator(Tree2D tree) {
+	  double distance = 0;
+	  Point root = tree.getRoot();
+	  for(Tree2D subTree : tree.getSubTrees()) {
+		  distance += root.distance(subTree.getRoot());
+		  distance += distanceCalculator(subTree);
+	  }
+	  return distance;
   }
-  
-  public ArrayList<Point> Greedy(ArrayList<PointPath> paths, int budget){
-	System.out.println("greedy running");
-	HashMap<Double,PointPath> map = new HashMap<Double,PointPath>();
-	for(PointPath path: paths) {
-		Double ratioed = path.distance/budget;
-		map.put(ratioed,path);
-	}
-    List<Double> weightList = new ArrayList<>(map.keySet());
-    Collections.sort(weightList);
-    
-	double sum = 0;
-	ArrayList<Point> resultPoints = new ArrayList<Point>();
-	for(Double weight : weightList) {
-		if(sum + weight <= 1) {
-			sum += weight;
-			resultPoints.add(map.get(weight).hitPoint);
-		}
-	}
-	for(Double weight : map.keySet()) {
-		System.out.println("hitPoint : " + map.get(weight).hitPoint);
-		System.out.println("weight : " + weight);
-	} 
-	
-	System.out.println("Result size : "+ resultPoints.size());
-	System.out.println("greedy finished");
-	return resultPoints;
+  public ArrayList<infoPoint> scoring_algorithm(ArrayList<Point> points, ArrayList<Point> hitPoints, int[][] shortestPaths, int threshold) {
+	  ArrayList<infoPoint> infoPoints = new ArrayList<infoPoint>();
+	  
+	  ArrayList<PointPath> paths = new ArrayList<PointPath>();
+	  int nbClose = 0;
+	  for(Point start : hitPoints) {
+		  paths.clear();
+		  nbClose = 0; // reset
+		  for(Point end : hitPoints) {
+			  if(start.equals(end)) continue;
+			  PointPath path = getPath(points,shortestPaths,start,end);
+			  if(path.distance <= threshold) {
+				  nbClose++;
+			  }
+			  paths.add(path);
+		  }
+		  infoPoint info = new infoPoint(start, paths, nbClose);
+		  infoPoints.add(info);
+	  }
+	  
+	  return infoPoints;
+  }
+  public PointPath getPath(ArrayList<Point> points, int[][] paths ,Point start, Point end) {
+		Point startPoint = start;
+    	Point nextPoint = new Point(-1,-1); // point that cant exist
+    	Point endPoint = end;
+    	int end_point_Index = points.indexOf(end);
+    	double distance = 0;
+    	ArrayList<Point> path = new ArrayList<Point>();
+    	
+    	path.add(startPoint);
+    	while(!nextPoint.equals(endPoint)) {
+    		int nextPoint_Index = paths[points.indexOf(startPoint)][end_point_Index];
+    		nextPoint = points.get(nextPoint_Index);
+    		
+    		path.add(nextPoint);
+    		distance += startPoint.distance(nextPoint);
+    		
+    		startPoint = nextPoint;
+
+    	}
+    	return new PointPath(start, end, path, distance);
   }
   
   class PointPath{
-	  private Point hitPoint;
+	  private Point startPoint;
+	  private Point endPoint;
 	  private ArrayList<Point> path;
 	  double distance;
-	  protected PointPath(Point hitPoint ,ArrayList<Point> path , double distance) {
-		  this.hitPoint = hitPoint;
+	  protected PointPath(Point startPoint, Point endPoint ,ArrayList<Point> path , double distance) {
+		  this.startPoint = startPoint;
+		  this.endPoint = endPoint;
 		  this.path = path;
 		  this.distance = distance;
 	  }
@@ -173,8 +181,49 @@ public class DefaultTeam {
 	  public double getDistance() {
 		  return distance;
 	  }
-	  public Point getHitPoint() {
-		  return hitPoint;
+	  public Point getStartPoint() {
+		  return startPoint;
+	  }
+	  public Point getEndPoint() {
+		  return endPoint;
+	  }
+  }
+  
+  class MapHolder{
+	  private HashMap<Point, ArrayList<PointPath>> mapPaths;
+	  private HashMap<Point , Integer> mapClose;
+	  MapHolder(HashMap<Point, ArrayList<PointPath>> mapPaths, HashMap<Point , Integer> mapClose){
+		  this.mapPaths = mapPaths;
+		  this.mapClose = mapClose;
+	  }
+	  
+	  public HashMap<Point, ArrayList<PointPath>> getMapPaths() {
+		  return mapPaths;
+	  }
+	  public HashMap<Point , Integer> getMapClose(){
+		  return mapClose;
+	  }
+  }
+  
+  class infoPoint{
+	  private Point root;
+	  private ArrayList<PointPath> paths;
+	  private int nb_closePoints;
+	  
+	  infoPoint(Point root , ArrayList<PointPath> paths, int closePoints){
+		  this.root = root;
+		  this.paths = paths;
+		  this.nb_closePoints = closePoints;
+	  }
+	  
+	  public Point getRoot() {
+		  return root;
+	  }
+	  public ArrayList<PointPath> getPaths(){
+		  return paths;
+	  }
+	  public int getClosePointsCount() {
+		  return nb_closePoints;
 	  }
   }
 }
